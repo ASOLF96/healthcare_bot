cc08c737b90b1ee7e4cbd0a8170bf61180628dbc
Asdfyash1 <Asdfyash1@users.noreply.github.com>
2026-03-24 19:21:03 +0530

    Fix 404 for root path and favicon by adding view routing and favicon image

--- healthcare_bot/urls.py
+++ healthcare_bot/urls.py
@@ -4,6 +4,8 @@ healthcare_bot URL Configuration
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.templatetags.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,6 @@ urlpatterns = [
     path('chat/', include('chatbot.urls')),
     path('location/', include('location.urls')),
     path('admin-panel/', include('admin_panel.urls')),
+    path('', RedirectView.as_view(url='/chat/', permanent=False), name='home'),
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.jpeg'), permanent=True)),
 ]
